c675bdd9e0cf48691af14e304db03119d30e1055
root <emploi.neveu@gmail.com>
2018-11-21 10:15:34 +0100

    exo3

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+    function phrase($String,$String1)
+    {
+      return $String . $String1;
+    }
+    echo phrase('Hello','World');
+    ?>
+  </body>
+</html>
